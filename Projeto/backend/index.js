bc8462ff31a6eb0786289a09bf8d4c2ba6fe51e4
Heygon Lago <heygonn@gmail.com>
2024-07-18 22:20:22 -0300

    Carregando o projeto

---
+++ Projeto/backend/index.js
@@ -0,0 +1,30 @@
+const { DBFFile } = require('dbffile');
+var shapefile = require("shapefile");
+
+async function readDBF() {
+    let dbf = await DBFFile.open('./ibge_municipio.dbf');
+    console.log(`DBF file contains ${dbf.recordCount} records.`);
+    console.log(`Field names: ${dbf.fields.map(f => f.name).join(', ')}`);
+    for await (const record of dbf) console.log(record);
+}
+
+
+
+async function readSHP() {
+shapefile.open("./ibge_municipio.shp")
+    .then(source => source.read()
+        .then(function log(result) {
+            if (result.done) return;
+            console.log(result.value.geometry.coordinates);
+            /* result.value.geometry.coordinates[0].map((e) => {
+                console.log(e)
+            }) */
+            return source.read().then(log);
+        }))
+    .catch(error => console.error(error.stack));
+}
+
+
+
+//readSHP()
+readDBF()
