2a0acd1b0bf4a803d5fde3f5df60533046b9db8c
Heygon Lago <heygonn@gmail.com>
2024-08-29 01:52:40 -0300

    Ajustando as rotas pra funcionar no build

--- Projeto/frontend/src/App.tsx
+++ Projeto/frontend/src/App.tsx
@@ -1,37 +1,5 @@
-import Home from './screens/Home';
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Mapas from './screens/Mapas';
-import Comunicacao from './screens/Comunicacao';
-import FaleConosco from './screens/FaleConosco';
-import Noticia from './screens/Noticia';
-//import NoMatch from './screens/NoMatch';
-
-import Entrar from './screens/Entrar';
-
-
-const router = createBrowserRouter([
-  { path: "/", element : <Home /> },
-  { path: "/index.html", element : <Home /> },
-  { path: "/entrar", element : <Entrar /> },
-  { path: "/mapas", element : <Mapas /> },
-  { path: "/comunicacao", element : <Comunicacao /> },
-  { path: "/faleconosco", element : <FaleConosco /> },
-  { path: "/noticia/:id", element : <Noticia /> }
-])
-
-
-
-/*
-{ path: "/", element : <Home />, errorElement: <NoMatch /> },
-{ path: "/entrar", element : <Entrar />, errorElement: <NoMatch /> },
-{ path: "/mapas", element : <Mapas />, errorElement: <NoMatch /> },
-{ path: "/comunicacao", element : <Comunicacao />, errorElement: <NoMatch /> },
-{ path: "/faleconosco", element : <FaleConosco />, errorElement: <NoMatch /> },
-{ path: "/noticia/:id", element : <Noticia />, errorElement: <NoMatch /> }
-*/
-
+import { Outlet } from "react-router-dom"
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return <Outlet />
 }
--- Projeto/frontend/src/main.tsx
+++ Projeto/frontend/src/main.tsx
@@ -2,8 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './screens/Home';
+import Mapas from './screens/Mapas';
+import Comunicacao from './screens/Comunicacao';
+import FaleConosco from './screens/FaleConosco';
+import Noticia from './screens/Noticia';
+import NoMatch from './screens/NoMatch';
+import Entrar from './screens/Entrar';
+
+
+
+const router = createBrowserRouter([
+  { path: "/", element : <App />,
+    errorElement : <NoMatch />,
+    children : [
+      { path: "/", element : <Home /> },
+      { path: "entrar", element : <Entrar /> },
+      { path: "mapas", element : <Mapas /> },
+      { path: "comunicacao", element : <Comunicacao /> },
+      { path: "faleconosco", element : <FaleConosco /> },
+      { path: "noticia/:id", element : <Noticia /> }
+    ]
+  },
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
